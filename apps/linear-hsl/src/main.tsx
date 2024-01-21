e516fabb84f04ee442aedd413ab2f0fefc3f5e2d
Tomasz Swistak <swistak.tomasz@outlook.com>
2024-01-21 09:06:36 +0100

    added linear-hsl presentation

---
+++ apps/linear-hsl/src/main.tsx
@@ -0,0 +1,13 @@
+import PresentationsGradient from '@gradient/presentation-gradient';
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <StrictMode>
+    <PresentationsGradient type="linearHsl" />
+  </StrictMode>
+);
